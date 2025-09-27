0fb3f59ad26d40edaeae76c9fc0e4edfd90c37a6
JoyceMaidd <joycemickeyfish@gmail.com>
2025-09-27 14:50:02 -0400

    Implement the 8-bit synchronous tri-state output counter

--- src/project.v
+++ src/project.v
@@ -16,12 +16,35 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  //control signals
+  wire load = uio_in[0];
+  wire count_enable = uio_in[1];
+  wire output_enable = uio_in[2];
+
+  //8-bit counter register
+  reg [7:0] count;
+
+  //Synchronous logic
+  always @(posedge clk or negedge rst_n) begin
+    //reset
+    if(!rst_n)
+      count <= 8'b0;
+    //load the input
+    else if(load)
+      count <= ui_in;
+    //start counting
+    else if(count_enable)
+      count <= count + 1'b1;
+  end
+
+  //tri-state output
+  assig uo_out = output_enable ? count : 8'bz
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
